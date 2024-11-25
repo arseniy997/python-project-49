2fccdbbfbf6ac9314f671613b746137f89be4676
Arseniy Grekhov <arseniy997@gmail.com>
2024-11-25 19:53:04 +0100

    Step 8: Add game_4

---
+++ brain_games/games/game_4.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+import random
+from brain_games.games.game_mechanic import game_mechanic
+
+
+''' Game 4 - “Arithmetic progression”. The user is shown
+a list of mnumbers with 1 number missing and must write this number.
+
+The game is finished once a user gives 3 correct answers
+or 1 incorrect answer.'''
+
+
+RULES_GAME_4 = 'What number is missing in the progression?'
+
+
+def generate_question_answer_game_4():
+    progression_length = random.randint(5, 10)
+    first_number = random.randint(1, 50)
+    step = random.randint(1, 10)
+    progression = [str(first_number)]
+
+    for i in range(1, progression_length):
+        y = int(progression[-1]) + step
+        progression.append(str(y))
+
+    correct_answer = progression[random.randint(0, (progression_length - 1))]
+    progression[progression.index(correct_answer)] = '..'
+
+    return [' '.join(progression), correct_answer]
+
+
+def main():
+    game_mechanic(RULES_GAME_4, generate_question_answer_game_4)
+
+
+if __name__ == '__main__':
+    main()
---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import brain_games.games.game_4
+
+
+'''This script runs game_4 - "Algorithmic progression".'''
+
+
+def main():
+    brain_games.games.game_4.main()
+
+
+if __name__ == '__main__':
+    main()
